3c229e4041d4d8b49cb5deeade4dbc3df9544b12
NthTensor <nth.tensor@gmail.com>
2020-03-22 15:29:44 -0500

    Implemented socketio-redis

--- bin/dev.js
+++ bin/dev.js
@@ -1,17 +1,34 @@
 'use strict';
 
 const cluster = require('cluster');
-const coreCount = require('os').cpus().length;
+const debug = require('debug')('app:server');
 
 if (cluster.isMaster) {
-	new Array(coreCount).fill(true).forEach(() => {
-		cluster.fork();
-	});
-} else {
-	console.log(`Worker ${process.pid} started`);
+	const net = require('net');
+	const farmhash = require('farmhash');
+	const coreCount = require('os').cpus().length;
 
-	const http = require('http');
-	const express = require('express');
+	/* Distributes an IP to a number of workers using a really fast hash. */
+	const worker_index = function(ip, len) {
+		return farmhash.fingerprint32(ip) % len;
+	};
+
+	let workers = [];
+
+	/* Spawns a worker process that will auto-restart. */
+	const spawn = function(i) {
+		workers[i] = cluster.fork();
+
+		workers[i].on('exit', function(code, signal) {
+			console.log('respawning worker', i);
+			spawn(i);
+		});
+	};
+
+	for (let i = 0; i < coreCount; i++) {
+		spawn(i);
+	}
+
 	const port = (() => {
 		const val = process.env.PORT || '8080';
 		const port = parseInt(val, 10);
@@ -27,19 +44,17 @@ if (cluster.isMaster) {
 		return false;
 	})();
 
-	require('dotenv').config();
-
-	global.app = express();
-
-	const debug = require('debug')('app:server');
-	const server = http.createServer(app);
-
-	global.io = require('socket.io')(server);
-	global.notify = require('node-notifier');
-
-	app.set('port', port);
-	app.set('strict routing', true);
-	server.listen(port);
+	/* Proxy socket.io connections through to workers. */
+	const server = net
+		.createServer({ pauseOnConnect: true }, function(connection) {
+			/* We received a connection and need to pass it to the appropriate
+			 * worker. Get the worker for this connection's source IP and pass
+			 * it the connection.
+			 */
+			const worker = workers[worker_index(connection.remoteAddress, coreCount)];
+			worker.send('sticky-session:connection', connection);
+		})
+		.listen(port);
 
 	function onError(error) {
 		if (error.syscall !== 'listen') {
@@ -67,9 +82,54 @@ if (cluster.isMaster) {
 		const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 		debug('Listening on ' + bind);
 		console.log('Listening on ' + bind);
-		require('../app');
 	}
 
 	server.on('error', onError);
+	server.on('listening', onListening);
+
+	/* Now do nothing.
+	 * Anything else increases the chances of crashing the master thread.
+	 */
+} else {
+	const http = require('http');
+	const express = require('express');
+	const socket_redis = require('socket.io-redis');
+
+	require('dotenv').config();
+
+	global.app = express();
+
+	const server = http.createServer(app);
+
+	global.io = require('socket.io')(server);
+	global.notify = require('node-notifier');
+
+	app.set('strict routing', true);
+
+	/* Dont expose our server publicly,
+	 * We expose the master process and it will route for the children. */
+	server.listen(0, 'localhost');
+
+	/* Enable socket.io redis */
+	io.adapter(socket_redis({ host: 'localhost', port: 6379 }));
+
+	/* Listen to messages sent from the master. */
+	process.on('message', function(message, connection) {
+		if (message !== 'sticky-session:connection') {
+			return;
+		}
+
+		/* Emulate a connection event on the server by emitting the
+		 * event with the connection the master sent us.
+		 */
+		server.emit('connection', connection);
+		connection.resume();
+	});
+
+	/* Once the server is up, start the application logic */
+	function onListening() {
+		require('../app');
+	}
+
 	server.on('listening', onListening);
 }
